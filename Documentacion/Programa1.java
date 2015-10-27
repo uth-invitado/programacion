bd87d0046487ecd353b3ea9204b9efc8cee3da5f
uth-tics <uth.tics.tareas@gmail.com>
2015-10-27 11:33:01 -0600

    Update Programa1.java
    Ingresar comentario en lineas 5-10

--- Documentacion/Programa1.java
+++ Documentacion/Programa1.java
@@ -2,7 +2,12 @@ public class Programa1{
 	
 	private int propiedad1;
 	public Programa1(){
+		// Mi cambio
 		
+		// aqui tambio realiza un cambio donde otro usuario tambien escribe
+		
+		
+		// desde el maste
 		
 	}
 	
